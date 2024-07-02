3f4015792e6b8903d1ced06477053ab6f8c16733
Reshma <reshmashaik0910@gmail.com>
2024-07-02 10:00:28 +0100

    changed categories file

--- src/Categories.js
+++ src/Categories.js
@@ -1,34 +1,22 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
-const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+import React from 'react';
 
-function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
-
-  const filterItems = (category) => {
-    if (category === 'all') {
-      setMenuItems(items);
-      return;
-    }
-    const newItems = items.filter((item) => item.category === category);
-    setMenuItems(newItems);
-  };
-
+const Categories = ({ categories, filterItems }) => {
   return (
-    <main>
-      <section className="menu section">
-        <div className="title">
-          <h2>our menu</h2>
-          <div className="underline"></div>
-        </div>
-        <Categories categories={categories} filterItems={filterItems} />
-        <Menu items={menuItems} />
-      </section>
-    </main>
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
   );
-}
+};
 
-export default App;
+export default Categories;
