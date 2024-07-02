6ea07879d41124481534ea5fa9c011a4a899b10a
Reshma <reshmashaik0910@gmail.com>
2024-07-02 19:49:31 +0100

    Added image

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ const menu = [
     title: 'diner double',
     category: 'lunch',
     price: 13.99,
-    img: './images/pouring-honey-on-pancakes.jpeg',
+    img: './images/diner-double.avif',
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
